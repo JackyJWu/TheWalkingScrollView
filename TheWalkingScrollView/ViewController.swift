845122496e2ca3cdcb387c3e75f9f69662951913
Jacky <jackyjwu86@gmail.com>
2018-01-06 17:59:19 -0700

    Initial Commit

---
+++ TheWalkingScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheWalkingScrollView
+//
+//  Created by Jacky on 2018-01-06.
+//  Copyright © 2018 Jacky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
